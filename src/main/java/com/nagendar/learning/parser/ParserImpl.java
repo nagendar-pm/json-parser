fdc7a186d027d5d0ec1cbec688fdb93d48e21a09
pagidimarri_nagendar <nagender.pm48@gmail.com>
2023-10-11 22:00:29 +0530

    completed addition of a valid parser

--- src/main/java/com/nagendar/learning/parser/ParserImpl.java
+++ src/main/java/com/nagendar/learning/parser/ParserImpl.java
@@ -30,33 +30,57 @@ public class ParserImpl implements Parser {
 				symbols.pop();
 				symbols.push(new Lexeme(DataType.ARRAY, list));
 			} else if (lexemes.get(i).getTokenType() == Brace.RIGHT_BRACE) {
-				Map<Lexeme, Lexeme> lexemeMap = new LinkedHashMap<>();
+				Map<String, Object> lexemeMap = new LinkedHashMap<>();
 				while (!symbols.isEmpty() &&
 						symbols.peek().getTokenType() != Brace.LEFT_BRACE) {
-					// TODO: handle this
-					symbols.pop();
+					if (symbols.peek().getTokenType() == Comma.COMMA) {
+						symbols.pop();
+					}
+					List<Lexeme> pair = handlePair(symbols);
+					if (Objects.isNull(pair)) {
+						// throw exception here
+						continue;
+					}
+					lexemeMap.put(pair.get(0).getValue().toString(), pair.get(1));
 				}
 				symbols.pop();
 				symbols.push(new Lexeme(DataType.OBJECT, lexemeMap));
-			} else if (lexemes.get(i).getTokenType() == Comma.COMMA) {
-				if (symbols.size() < 3) {
-					// TODO: throw an exception here
-					System.out.println("invalid json");
-				}
-				Lexeme valueCandidate = symbols.pop();
-				Lexeme colonCandidate = symbols.pop();
-				if (colonCandidate.getTokenType() != Colon.COLON) {
-					// Comma possibly belongs to an array
-					symbols.push(colonCandidate);
-					symbols.push(valueCandidate);
-				}
-				Lexeme keyCandidate = symbols.pop();
-				// TODO: we can push these lexemes into a map kind
 			}
+//			else if (lexemes.get(i).getTokenType() == Comma.COMMA) {
+//				List<Lexeme> pair = handlePair(symbols);
+//				if (Objects.isNull(pair)) continue;
+//				json.put(pair.get(0).getValue().toString(), pair.get(1));
+//			}
 			else {
 				symbols.push(lexemes.get(i));
 			}
 		}
+		if (symbols.size() == 1) {
+			Lexeme jsonObject = symbols.peek();
+			Map<String, Object> json = (Map<String, Object>) jsonObject.getValue();
+			List<String> reverseOrderKeys = new ArrayList<>(json.keySet());
+			Collections.reverse(reverseOrderKeys);
+			Map<String, Object> jsonCorrect = new LinkedHashMap<>();
+			reverseOrderKeys.forEach(key -> jsonCorrect.put(key, json.get(key)));
+			System.out.println("jsonCorrect = " + jsonCorrect);
+		}
 		return symbols.size() == 1 && symbols.get(0).getTokenType() == DataType.OBJECT;
+	}
+
+	private List<Lexeme> handlePair (Stack<Lexeme> symbols) {
+		if (symbols.size() < 3) {
+			// TODO: throw an exception here
+			System.out.println("invalid json");
+		}
+		Lexeme valueCandidate = symbols.pop();
+		Lexeme colonCandidate = symbols.pop();
+		if (colonCandidate.getTokenType() != Colon.COLON) {
+			// Comma possibly belongs to an array
+			symbols.push(colonCandidate);
+			symbols.push(valueCandidate);
+			return null;
+		}
+		Lexeme keyCandidate = symbols.pop();
+		return List.of(keyCandidate, valueCandidate);
 	}
 }
