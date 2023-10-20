b44294827d3ddc2c7ef1d943fee17851ccfc10c3
pagidimarri_nagendar <nagender.pm48@gmail.com>
2023-10-20 22:38:35 +0530

    integrated the support to test multiple files for json validation

--- src/main/java/com/nagendar/learning/Main.java
+++ src/main/java/com/nagendar/learning/Main.java
@@ -20,19 +20,41 @@ import com.nagendar.learning.parser.analyzer.AnalyzerFactory;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		String inputFile = "/Users/pagidimarrinagendar/Documents/My/java-projects/coding-challenges/json-parser/resources/input.txt";
-		Path file = Path.of(inputFile);
-		String fileContent = Files.readString(file);
-		System.out.println("fileContent = " + fileContent);
+		String dir = "/Users/pagidimarrinagendar/Documents/My/java-projects/coding-challenges/json-parser/resources";
+		Set<String> inputFiles = listFilesUsingFilesList(dir);
+		for (String inputFile : inputFiles) {
+			Path file = Path.of(inputFile);
+			String fileContent = Files.readString(file);
+			System.out.println("Working on " + inputFile);
+			System.out.println("fileContent = " + fileContent);
+			List<Lexeme> lexemes = tokenize(fileContent);
+			analyze(lexemes, new Input(fileContent));
+			parse(lexemes);
+		}
+	}
+
+	private static Set<String> listFilesUsingFilesList(String dir) throws IOException {
+		try (Stream<Path> stream = Files.list(Paths.get(dir))) {
+			return stream
+					.filter(file -> !Files.isDirectory(file))
+					.map(Path::toString)
+					.collect(Collectors.toSet());
+		}
+	}
+
+	private static List<Lexeme> tokenize(String fileContent) {
 		StringBuilder stringBuilder = new StringBuilder();
 		TokenizerFactory tokenizerFactory = new TokenizerFactory(stringBuilder);
 		Lexer lexer = new LexerImpl(fileContent, tokenizerFactory);
-		Parser parser = new ParserImpl();
 		List<Lexeme> lexemes = new ArrayList<>();
 		while (lexer.hasToken()) {
 			Lexeme lexeme = lexer.nextToken();
@@ -40,10 +62,18 @@ public class Main {
 			lexemes.add(lexeme);
 			System.out.println(lexeme);
 		}
+		return lexemes;
+	}
+
+	private static void analyze(List<Lexeme> lexemes, Input input) {
 		TokenBase tokenBase = new TokenBase(lexemes);
-		AnalyzerFactory analyzerFactory = new AnalyzerFactory(new Input(fileContent));
+		AnalyzerFactory analyzerFactory = new AnalyzerFactory(input);
 		Analyzer analyzer = analyzerFactory.getAnalyzerForToken(tokenBase.getLexeme().getTokenType());
 		analyzer.analyze(tokenBase);
+	}
+
+	private static void parse(List<Lexeme> lexemes) {
+		Parser parser = new ParserImpl();
 		boolean parse = parser.parse(lexemes);
 		System.out.println("Verdict = " + (parse ? "Valid JSON" : "Invalid JSON"));
 	}
